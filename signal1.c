d958f7dfec5b10d76e13cceb3bde7870891491fb
SuranjanDaw <suranjandaw@gmail.com>
2019-11-24 21:32:03 +0530

    signals added

--- signal1.c
+++ signal1.c
@@ -1,5 +1,29 @@
 #include<stdio.h>
-#include<signals.h>
+#include<signal.h>
 #include<unistd.h>
 #include<sys/wait.h>
 #include<sys/types.h>
+
+void func(int x)
+{
+  signal(SIGINT,SIG_DFL);
+  printf("SIGINT recieved= %d\n",x );
+}
+int main()
+{
+  pid_t pid;
+  signal(SIGINT,func);
+  if((pid=fork())==0)
+  {
+     printf("CHILD(%d) my parent is %d\n",getpid(),getppid() );
+     while(1){
+       sleep(2);
+       printf("in loop\n" );
+     }
+     sleep(2);
+  }else if(pid > 0)
+  {
+    printf("PARENT(%d) my CHILD is %d\n",getpid(),pid );
+    wait(NULL);
+  }
+}
